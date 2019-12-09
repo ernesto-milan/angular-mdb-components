a8c2bc542d2b3107b48c2c052e4735a5c43e0d63
Ernesto Milan <ernesto.milan.em@gmail.com>
2019-12-09 23:33:32 +0100

    adding routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
+import { ChiSiamoComponent } from './page/chi-siamo/chi-siamo.component';
+import { AmbienteComponent } from './page/ambiente/ambiente.component';
+import { OfferteComponent } from './page/offerte/offerte.component';
+import { CamereComponent } from './page/camere/camere.component';
+import { HomeComponent } from './page/home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"home", component:HomeComponent},
+  {path:"camere", component:CamereComponent},
+  {path:"offerte", component:OfferteComponent},
+  {path:"ambiente", component:AmbienteComponent},
+  {path:"chiSiamo", component:ChiSiamoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
