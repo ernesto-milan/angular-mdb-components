2bec71c823f9c60b792f8bc112bc82b1e05b2728
Ernesto Milan <ernesto.milan.em@gmail.com>
2019-12-10 15:12:28 +0100

    fixing route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 
 const routes: Routes = [
+  {path: "", pathMatch: "full", redirectTo: "home" },
   {path:"home", component:HomeComponent},
   {path:"camere", component:CamereComponent},
   {path:"offerte", component:OfferteComponent},
