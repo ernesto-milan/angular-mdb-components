551a1ab01afeed10fbb62265e29d586a94a5b4d0
Ernesto Milan <ernesto.milan.em@gmail.com>
2019-12-10 02:04:42 +0100

    changing page offerte

--- src/app/page/offerte/offerte.component.ts
+++ src/app/page/offerte/offerte.component.ts
@@ -1,3 +1,5 @@
+import { JumboComponent } from './../../content/jumbo/jumbo.component';
+import { CardComponent } from './../../content/card/card.component';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
